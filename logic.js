e2418308eca69bbebee6613592950679e056d872
Zablackafro <zahidabdikadir12@gmail.com>
2020-10-08 12:49:25 -0500

    did some more problems

--- logic.js
+++ logic.js
@@ -43,3 +43,66 @@ const reverse = (str) => {
 };
 
 console.log(reverse('sheep'));
+
+// 4. nth Triangular Number
+// Calculate the nth triangular number. A triangular number counts the objects that can form an equilateral triangle. The nth triangular number is the number of dots composing a triangle with n dots on a side, and is equal to the sum of the n natural numbers from 1 to n. This is the Triangular Number Sequence: 1, 3, 6, 10, 15, 21, 28, 36, 45.
+
+const nth = (n) => {
+  // base case
+  if (n === 1) {
+    return 1;
+  }
+  // recursion
+
+  return n + nth(n - 1);
+};
+
+console.log(nth(4)); //10
+
+// 5. String Splitter
+// Write a recursive function that splits a string based on a separator (similar to String.prototype.split). Don't use JS array's split function to solve this problem. Input: 02/20/2020 Output: ["02", "20", "2020"]
+
+const split = (str) => {};
+
+// 6. Fibonacci
+// Write a recursive function that prints the Fibonacci sequence of a given number. The Fibonacci sequence is a series of numbers in which each number is the sum of the 2 preceding numbers. For example, the 7th Fibonacci number in a Fibonacci sequence is 13. The sequence looks as follows: 1, 1, 2, 3, 5, 8, 13.
+const fib = (num) => {
+  // base case
+  if (num <= 0) {
+    return 0;
+  } else if (num === 1) {
+    return 1;
+  }
+  // recursion
+  return fib(num - 1) + fib(num - 2);
+};
+
+console.log(fib(7)); // 13
+
+// 7. Factorial
+// Write a recursive function that finds the factorial of a given number. The factorial of a number can be found by multiplying that number by each number between itself and 1. For example, the factorial of 5 is 5 * 4 * 3 * 2 * 1 = 120.
+const factorial = (num) => {
+  // base case
+  if (num === 1) {
+    return 1;
+  }
+  // recursion
+  return num * factorial(num - 1);
+};
+
+console.log(factorial(5)); //120
+// 8. Find a way out of the maze
+// You have entered a Maze and need to find your way out of it. There are more than one possible paths through the Maze to the single exit point. Write a recursive function that will help you find a possible path through the maze.
+// You can use the following mazes to test your program:
+let mySmallMaze = [
+  [' ', ' ', ' '],
+  [' ', '*', ' '],
+  [' ', ' ', 'e'],
+];
+let maze = [
+  [' ', ' ', ' ', '*', ' ', ' ', ' '],
+  ['*', '*', ' ', '*', ' ', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', '*', '*', '*', '*', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', 'e'],
+];
